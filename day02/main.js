fbda223daeed05a36d78d9d705ceebebeb0766a5
sontv8 <sontv8@fpt.edu.vn>
2023-03-10 10:36:17 +0700

    Buổi 2: ôn tập interface

--- day02/main.js
+++ day02/main.js
@@ -1,3 +1,36 @@
 "use strict";
-// exports.__esModule = true;
+exports.__esModule = true;
 var username = "sontv";
+var isAdmin = true;
+/*
+    Mảng số
+    Mảng chuỗi
+    Mảng mix
+*/
+var arrNumber = [1, 2, 3, 4];
+var arrStr = ["abc", "def"];
+var arrMix = [123, "abc"];
+var project = {
+    id: 1,
+    name: "Du an mau",
+    image: "http://...",
+    link: "http://...",
+    status: true
+};
+var showProject = function (props) {
+    return props;
+};
+console.log(showProject(project));
+var getProject = function (data) {
+    console.log(data);
+};
+var projectList = [
+    {
+        id: 1,
+        name: "Du an mau",
+        image: "http://...",
+        link: "http://...",
+        status: true
+    }
+];
+getProject(projectList);
--- day02/main.ts
+++ day02/main.ts
@@ -11,11 +11,68 @@ const arrNumber: number[] = [1, 2, 3, 4]
 const arrStr: string[] = ["abc", "def"]
 const arrMix: Array<number | string> = [123, "abc"]
 
-const project: { id: number, name: string } = {
-    id: 1,
-    name: "Du an mau"
+
+// const projectList:{id:number,name:string}[] = [
+//     { id: 1, name: "Du an mau" },
+//     { id: 2, name: "Du an 1" }
+// ]
+
+// const projectList: Array<{ id: number, name: string }> = [
+//     { id: 1, name: "Du an mau" },
+//     { id: 2, name: "Du an 1" }
+// ]
+
+// interface IProject {
+//     id: number,
+//     name: string,
+//     image: string,
+//     link: string,
+//     status: boolean
+// }
+// const project: IProject = {
+//     id: 1,
+//     name: "Du an mau",
+//     image: "http://...",
+//     link: "http://...",
+//     status: true
+// }
+// const showProject = (props: IProject): IProject => {
+//     return props
+// }
+// console.log(showProject(project));
+
+// const getProject = (data: IProject[]) => {
+//     console.log(data);
+
+// }
+
+
+interface IProject {
+    id: number,
+    name: string,
+    image: string,
+    link: string,
+    status: boolean
 }
-const projectList = [
-    { id: 1, name: "Du an mau" },
-    { id: 2, name: "Du an 1" }
+const projectList: IProject[] = [
+    {
+        id: 1,
+        name: "Du an mau",
+        image: "http://...",
+        link: "http://...",
+        status: true
+    },
+    {
+        id: 2,
+        name: "Du an 1",
+        image: "http://...",
+        link: "http://...",
+        status: false
+    }
 ]
+
+const showProject = (projects) => {
+    // tìm tới thẻ div có id app và hiển thị dữ liệu của mảng ra
+    // sử dụng map và hiển thị
+}
+showProject(projectList)
