f82355a20ba07afe057b3f6f3c18be914c5e3ab6
sontv8 <sontv8@fpt.edu.vn>
2023-03-10 09:52:10 +0700

    Buổi 2: ôn tập kiến thức

---
+++ day02/main.ts
@@ -0,0 +1,21 @@
+export { }
+let username: string = "sontv"
+let isAdmin: boolean = true
+
+/*
+    Mảng số
+    Mảng chuỗi
+    Mảng mix
+*/
+const arrNumber: number[] = [1, 2, 3, 4]
+const arrStr: string[] = ["abc", "def"]
+const arrMix: Array<number | string> = [123, "abc"]
+
+const project: { id: number, name: string } = {
+    id: 1,
+    name: "Du an mau"
+}
+const projectList = [
+    { id: 1, name: "Du an mau" },
+    { id: 2, name: "Du an 1" }
+]
