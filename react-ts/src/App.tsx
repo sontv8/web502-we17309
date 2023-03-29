1290b29ddd9a5b15053f58a120a1cccb2a1c4f1d
sontv8 <sontv8@fpt.edu.vn>
2023-03-29 10:51:35 +0700

    Buổi 11: product page

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -1,13 +1,27 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { Routes, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage'
+import { getAllProduct, deleteProduct, addProduct, updateProduct } from './api/product'
+import ProductPage from './pages/Product'
+interface IProduct {
+  id: number,
+  name: string,
+  price: number
+}
 function App() {
-
+  const [products, setProduct] = useState<IProduct[]>([])
+  useEffect(() => {
+    getAllProduct().then(({ data }) => setProduct(data))
+  }, [])
+  const onHandleRemove = (id: number) => {
+    deleteProduct(id).then(() => setProduct(products.filter(product => product.id !== id)))
+  }
   return (
     <div className="App">
       <Routes>
         <Route path='/'>
-          <Route index element={<HomePage />}></Route>
+          <Route index element={<HomePage />} />
+          <Route path='products' element={<ProductPage products={products} onRemove={onHandleRemove} />} />
         </Route>
       </Routes>
     </div>
---
+++ react-ts/src/pages/Product.tsx
@@ -0,0 +1,44 @@
+import { useEffect, useState } from "react"
+interface IProduct {
+    id: number,
+    name: string,
+    price: number
+}
+interface IProps {
+    products: IProduct[],
+    onRemove: (id: number) => void
+}
+const ProductPage = (props: IProps) => {
+    /*
+        {
+            products: [
+                {},{}
+            ]
+            onRemove: (id) => void
+        }
+    */
+    const [data, setData] = useState<IProduct[]>([])
+    useEffect(() => {
+        setData(props.products)
+    }, [props])
+    const removeProduct = (id: number) => {
+        props.onRemove(id)
+    }
+    return (
+        <div>
+            <h1>Product Page</h1>
+            {
+                data.map(product => {
+                    return (
+                        <div key={product.id}>
+                            <h2>{product.name}</h2>
+                            <button onClick={() => removeProduct(product.id)}>Remove</button>
+                        </div>
+                    )
+                })
+            }
+        </div>
+    )
+}
+
+export default ProductPage
