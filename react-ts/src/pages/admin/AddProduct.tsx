e712e9d7d92882774e3a374d427d668de400d527
sontv8 <sontv8@fpt.edu.vn>
2023-03-31 10:08:01 +0700

    Buổi 12: định nghĩa kiểu dữ liệu cho useForm

--- react-ts/src/pages/admin/AddProduct.tsx
+++ react-ts/src/pages/admin/AddProduct.tsx
@@ -1,11 +1,19 @@
 import React from 'react'
-import { useForm } from 'react-hook-form' //import useForm hook
-
-const AddProductPage = (props) => {
-    const { register, handleSubmit } = useForm()
+import { useForm, SubmitHandler } from 'react-hook-form' //import useForm hook
+import { IProduct } from '../../types/product'
+interface IProps {
+    onAdd: (product: IProduct) => void
+}
+interface IFormInput {
+    id: number,
+    name: string,
+    price: number
+}
+const AddProductPage = (props: IProps) => {
+    const { register, handleSubmit } = useForm<IFormInput>()
     //register là hàm dể đăng ký các trường dữ liệu trong form
     //handleSubmit là hàm dể xử lý khi submit form
-    const onHandleSubmit = (data) => {
+    const onHandleSubmit: SubmitHandler<IFormInput> = (data: IProduct) => {
         props.onAdd(data);
     }
     return (
