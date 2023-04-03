3705f7bce4f4222d0beeab5ab7acd6067a021988
sontv8 <sontv8@fpt.edu.vn>
2023-04-03 09:51:19 +0700

    Buổi 12: ant design - add product

--- react-ts/src/pages/admin/AddProduct.tsx
+++ react-ts/src/pages/admin/AddProduct.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useForm, SubmitHandler } from 'react-hook-form' //import useForm hook
 import { IProduct } from '../../types/product'
+import { Button, Checkbox, Form, Input } from 'antd';
 interface IProps {
     onAdd: (product: IProduct) => void
 }
@@ -10,19 +11,61 @@ interface IFormInput {
     price: number
 }
 const AddProductPage = (props: IProps) => {
-    const { register, handleSubmit } = useForm<IFormInput>()
-    //register là hàm dể đăng ký các trường dữ liệu trong form
-    //handleSubmit là hàm dể xử lý khi submit form
-    const onHandleSubmit: SubmitHandler<IFormInput> = (data: IProduct) => {
-        props.onAdd(data);
-    }
+    // const { register, handleSubmit } = useForm<IFormInput>()
+    // //register là hàm dể đăng ký các trường dữ liệu trong form
+    // //handleSubmit là hàm dể xử lý khi submit form
+    // const onHandleSubmit: SubmitHandler<IFormInput> = (data: IProduct) => {
+    //     props.onAdd(data);
+    // }
+
+    const onFinish = (values: any) => {
+        props.onAdd(values);
+    };
+
+    const onFinishFailed = (errorInfo: any) => {
+        console.log('Failed:', errorInfo);
+    };
     return (
         <div>
-            <form action="" onSubmit={handleSubmit(onHandleSubmit)}>
+            <Form
+                name="basic"
+                labelCol={{ span: 8 }}
+                wrapperCol={{ span: 16 }}
+                style={{ maxWidth: 600 }}
+                initialValues={{ remember: true }}
+                onFinish={onFinish}
+                onFinishFailed={onFinishFailed}
+                autoComplete="off"
+            >
+                <Form.Item
+                    label="Product Name"
+                    name="name"
+                    rules={[{ required: true, message: 'Please input your username!' }]}
+                >
+                    <Input />
+                </Form.Item>
+
+                <Form.Item
+                    label="Product Price"
+                    name="price"
+                    rules={[{ required: true, message: 'Please input your password!' }]}
+                >
+                    <Input />
+                </Form.Item>
+
+                <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+                    <Button type="primary" htmlType="submit">
+                        Add New Product
+                    </Button>
+                </Form.Item>
+            </Form>
+
+
+            {/* <form action="" onSubmit={handleSubmit(onHandleSubmit)}>
                 <input type="text" {...register("name")} />
                 <input type="number" {...register("price")} />
                 <button type="submit">Add New Product</button>
-            </form>
+            </form> */}
         </div>
     )
 }
