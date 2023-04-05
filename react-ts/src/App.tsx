978efd4a4abb8510d404054b11e5253cb6b18216
sontv8 <sontv8@fpt.edu.vn>
2023-04-05 09:42:08 +0700

    Buổi 14: ant design - update product + product management

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -1,45 +1,49 @@
 import { useState, useEffect } from 'react'
 import { Routes, Route } from 'react-router-dom'
-import HomePage from './pages/HomePage'
-import { getAllProduct, deleteProduct, addProduct, updateProduct } from './api/product'
-import ProductPage from './pages/Product'
-import { IProduct } from './types/product'
-import ProductDetailPage from './pages/ProductDetail'
+import { addProduct, deleteProduct, getAllProduct, updateProduct } from './api/product'
 import AddProductPage from './pages/admin/AddProduct'
+import Dashboard from './pages/admin/Dashboard'
+import ProductManagementPage from './pages/admin/ProductManagement'
 import UpdateProductPage from './pages/admin/UpdateProduct'
+import HomePage from './pages/HomePage'
+import AdminLayout from './pages/layouts/AdminLayout'
+import ProductPage from './pages/Product'
+import ProductDetailPage from './pages/ProductDetail'
+import { IProduct } from './types/product'
+
 function App() {
-  const [products, setProduct] = useState<IProduct[]>([])
+  const [products, setProducts] = useState<IProduct[]>([])
   useEffect(() => {
-    getAllProduct().then(({ data }) => setProduct(data))
+    getAllProduct().then(({ data }) => setProducts(data))
   }, [])
   const onHandleRemove = (id: number) => {
-    deleteProduct(id).then(() => setProduct(products.filter(product => product.id !== id)))
+    deleteProduct(id).then(() => setProducts(products.filter((item: IProduct) => item.id !== id)))
   }
-  const onHandleAdd = (product) => {
-    addProduct(product)
+  const onHandleAdd = (product: IProduct) => {
+    addProduct(product).then(() => getAllProduct().then(({ data }) => setProducts(data)))
   }
-  const onHandleUpdate = (product) => {
-    updateProduct(product)
+  const onHandleUpdate = (product: IProduct) => {
+    updateProduct(product).then(() => getAllProduct().then(({ data }) => setProducts(data)))
   }
   return (
     <div className="App">
       <Routes>
         <Route path='/'>
           <Route index element={<HomePage />} />
-          <Route path='products'  >
-            <Route index element={<ProductPage products={products} onRemove={onHandleRemove} />} />
-            <Route path=':id' element={<ProductDetailPage products={products} />} />
-          </Route>
+          <Route path='products' element={<ProductPage products={products} onRemove={onHandleRemove} />} />
+          <Route path='products/:id' element={<ProductDetailPage products={products} />} />
         </Route>
-
-        <Route path='/admin'>
+        <Route path='/admin' element={<AdminLayout />}>
+          <Route index element={<Dashboard />} />
           <Route path='products'>
+            <Route index element={<ProductManagementPage products={products} onRemove={onHandleRemove} />} />
             <Route path='add' element={<AddProductPage onAdd={onHandleAdd} />} />
             <Route path=':id/update' element={<UpdateProductPage onUpdate={onHandleUpdate} products={products} />} />
           </Route>
         </Route>
       </Routes>
-    </div>
+
+    </div >
   )
 }
 
--- react-ts/src/pages/admin/AddProduct.tsx
+++ react-ts/src/pages/admin/AddProduct.tsx
@@ -1,7 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useForm, SubmitHandler } from 'react-hook-form' //import useForm hook
 import { IProduct } from '../../types/product'
-import { Button, Checkbox, Form, Input } from 'antd';
+import { Button, Form, Input, Modal, Upload } from 'antd';
+import { PlusOutlined } from '@ant-design/icons';
+import type { RcFile, UploadProps } from 'antd/es/upload';
+import type { UploadFile } from 'antd/es/upload/interface';
 interface IProps {
     onAdd: (product: IProduct) => void
 }
@@ -10,6 +13,13 @@ interface IFormInput {
     name: string,
     price: number
 }
+const getBase64 = (file: RcFile): Promise<string> =>
+    new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = () => resolve(reader.result as string);
+        reader.onerror = (error) => reject(error);
+    });
 const AddProductPage = (props: IProps) => {
     // const { register, handleSubmit } = useForm<IFormInput>()
     // //register là hàm dể đăng ký các trường dữ liệu trong form
@@ -19,12 +29,49 @@ const AddProductPage = (props: IProps) => {
     // }
 
     const onFinish = (values: any) => {
-        props.onAdd(values);
+        const newProduct = {
+            id: values.id,
+            name: values.name,
+            price: values.price,
+            image: values.image.file.name
+        }
+        console.log(values.image.file.name);
+
+        props.onAdd(newProduct);
     };
 
     const onFinishFailed = (errorInfo: any) => {
         console.log('Failed:', errorInfo);
     };
+
+    // upload image
+    const [previewOpen, setPreviewOpen] = useState(false);
+    const [previewImage, setPreviewImage] = useState('');
+    const [previewTitle, setPreviewTitle] = useState('');
+    const [fileList, setFileList] = useState<UploadFile[]>([]);
+
+    const handleCancel = () => setPreviewOpen(false);
+
+    const handlePreview = async (file: UploadFile) => {
+        if (!file.url && !file.preview) {
+            file.preview = await getBase64(file.originFileObj as RcFile);
+        }
+
+        setPreviewImage(file.url || (file.preview as string));
+        setPreviewOpen(true);
+        setPreviewTitle(file.name || file.url!.substring(file.url!.lastIndexOf('/') + 1));
+    };
+
+    const handleChange: UploadProps['onChange'] = ({ fileList: newFileList }) => setFileList(newFileList);
+
+    // setFileList(newFileList);
+
+    const uploadButton = (
+        <div>
+            <PlusOutlined />
+            <div style={{ marginTop: 8 }}>Upload</div>
+        </div>
+    );
     return (
         <div>
             <Form
@@ -51,6 +98,25 @@ const AddProductPage = (props: IProps) => {
                     rules={[{ required: true, message: 'Please input your password!' }]}
                 >
                     <Input />
+                </Form.Item>
+
+                <Form.Item
+                    label="Product Image"
+                    name="image"
+                    rules={[{ required: true, message: 'Please input your password!' }]}
+                >
+                    <Upload
+                        // action="http://localhost:3000/products"
+                        listType="picture-card"
+                        fileList={fileList}
+                        onPreview={handlePreview}
+                        onChange={handleChange}
+                    >
+                        {fileList.length >= 8 ? null : uploadButton}
+                    </Upload>
+                    {/* <Modal open={previewOpen} title={previewTitle} footer={null} onCancel={handleCancel}>
+                        <img alt="example" style={{ width: '100%' }} src={previewImage} />
+                    </Modal> */}
                 </Form.Item>
 
                 <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
